6ea9b4a02811020de27a13344bb8afc6cdfce618
williamcwi <williamcwi.chu@hotmail.com>
2023-12-09 13:00:31 +0000

    completed 1-2

--- day1.py
+++ day1.py
@@ -21,3 +21,19 @@ with open(os.path.join(INPUT_DIR, 'trebuchet.txt'), 'r', encoding='UTF-8') as f:
         answer += int(str(first(row)) + str(last(row)))
 
 print('Part 1 Answer: {}'.format(answer))
+
+with open(os.path.join(INPUT_DIR, 'trebuchet.txt'), 'r', encoding='UTF-8') as f:
+    answer = 0
+    for row in f:
+        newStr = re.sub(r'one',"o1e",row)
+        newStr = re.sub(r'two',"t2o",newStr)
+        newStr = re.sub(r'three',"t3e",newStr)
+        newStr = re.sub(r'four',"f4r",newStr)
+        newStr = re.sub(r'five',"f5e",newStr)
+        newStr = re.sub(r'six',"s6x",newStr)
+        newStr = re.sub(r'seven',"s7n",newStr)
+        newStr = re.sub(r'eight',"e8t",newStr)
+        newStr = re.sub(r'nine',"n9e",newStr)
+        answer += int(str(first(newStr)) + str(last(newStr)))
+
+print('Part 2 Answer: {}'.format(answer))
