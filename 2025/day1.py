5debb78b41946a13a315d0ebb21cd2b6c4f7c494
williamcwi <williamcwi.chu@hotmail.com>
2025-12-01 15:11:28 +0000

    Completed 2025 Day 1 Puzzles

--- 2025/day1.py
+++ 2025/day1.py
@@ -1,23 +1,20 @@
-import os
+import sys
 
-ROOT_DIR = os.path.abspath('./')
-INPUT_DIR = os.path.join(ROOT_DIR, 'inputs')
+lines = [line.strip() for line in sys.stdin.read().splitlines()]
 
-input_file = os.path.join(INPUT_DIR,'secret_entrance.txt')
-with open(input_file, 'r', encoding='UTF-8') as f:
-    lines = f.read().splitlines()
-
-results = 0
+p1 = 0
+p2 = 0
 dial = 50
 for l in lines:
     direction, number = l[0], int(l[1:])
+    for _ in range(number):
+        if direction == 'L':
+            dial = (dial - 1) % 100
+        elif direction == 'R':
+            dial = (dial + 1) % 100
+        if dial == 0:
+            p2 += 1
+    if dial == 0:
+        p1 += 1
 
-    if direction == 'L':
-        dial -= number
-    elif direction == 'R':
-        dial += number
-
-    if dial % 100 == 0:
-        results += 1
-
-print(f'Results: {results}')
+print(f'Part 1: {p1}\nPart 2: {p2}')
